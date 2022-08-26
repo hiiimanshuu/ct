4b1f83d475b6573f0f744af94098592be92b33fe
hiiimanshuu <110384655+hiiimanshuu@users.noreply.github.com>
2022-08-26 15:12:57 +0530

    Add files via upload

---
+++ question 1.cpp
@@ -0,0 +1,59 @@
+//NAME= HIMANSHU BHUGRA
+//ROLL NUMBER = 2010991890
+//SET = 4
+//QUESTION NUMBER 1
+
+
+
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+ 
+
+int findMinIndex(int arr[], int n)
+{
+    int minIndex = n;
+ 
+    
+    unordered_set<int> set;
+ 
+    
+    for (int i = n - 1; i >= 0; i--)
+    {
+        
+        if (set.find(arr[i]) != set.end()) {
+            minIndex = i;
+        }
+        
+        else {
+            set.insert(arr[i]);
+        }
+    }
+ 
+    
+    if (minIndex == n) {
+        return -1;
+    }
+ 
+    
+    return minIndex;
+}
+ 
+int main()
+{
+    int arr[] = { 5, 6, 3, 4, 3, 6, 4 };
+    
+ 
+    int n = sizeof(arr) / sizeof(arr[0]);
+ 
+    int minIndex = findMinIndex(arr, n);
+ 
+    if (minIndex != n) {
+        cout << "The minimum index of the repeating element is " << minIndex;
+    }
+    else {
+        cout << "Invalid Input";
+    }
+ 
+    return 0;
+}
